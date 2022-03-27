9fd78dab588a77b675f965b36d72c5ac30f1d35b
AbhineetGarg <62750126+AbhineetGarg@users.noreply.github.com>
2022-03-27 11:59:25 +0530

    Update deploy_emotion_detection.py

--- deploy_emotion_detection.py
+++ deploy_emotion_detection.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, Response
 app = Flask(__name__)
 
 def images():
-    frame = cv2.imread("C:/Users/Abhineet/Documents/DS 360 DigiTMG/Drowsiness Detection Project_Feb-22/disgust.jpg")
+    frame = cv2.imread("<image path>")
     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     predictions = DeepFace.analyze(frame, actions=['emotion'])
     cv2.putText(frame, predictions['dominant_emotion'], (20, 100), cv2.FONT_HERSHEY_COMPLEX, 3, (0, 255, 0), 2)
